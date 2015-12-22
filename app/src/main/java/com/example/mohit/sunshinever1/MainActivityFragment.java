69a993684c85b0b82ee11486c17f4d44836f5818
mohit30goel <mohit@smartcommercesolutions.in>
2015-12-22 09:50:25 +0530

    1.04_add_dummy_data

--- app/src/main/java/com/example/mohit/sunshinever1/MainActivityFragment.java
+++ app/src/main/java/com/example/mohit/sunshinever1/MainActivityFragment.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -16,7 +20,17 @@ public class MainActivityFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        String[] fakeData = {
+                "Today - Sunny-88/63",
+                "Tommorow - Foggy-70/46",
+                "Weds - Cloudy-72/63",
+                "Thurs - Rainy-64/51",
+                "Fri - Foggy-70/46",
+                "Sat - Sunny-76/68"
+        };
+        ArrayList<String> weekForecast = new ArrayList<>(Arrays.asList(fakeData));
+        return rootView;
     }
 }
