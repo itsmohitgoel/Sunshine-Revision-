6e7f945d271f72c8a499abeb47c469c5e07b0678
itsmohitgoel <mohit30goel@gmail.com>
2016-03-20 10:25:16 +0530

    2.01 Add the Network call code

--- app/src/main/java/com/example/mohit/sunshinever1/MainActivityFragment.java
+++ app/src/main/java/com/example/mohit/sunshinever1/MainActivityFragment.java
@@ -2,12 +2,19 @@ package com.example.mohit.sunshinever1;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,6 +24,7 @@ import java.util.List;
  */
 public class MainActivityFragment extends Fragment {
     ArrayAdapter<String> mForecastAdapter;
+    private static String LOG_TAG = MainActivityFragment.class.getSimpleName();
 
     public MainActivityFragment() {
     }
@@ -37,16 +45,60 @@ public class MainActivityFragment extends Fragment {
 
         //Create an arrayAdapter;
         mForecastAdapter = new ArrayAdapter<String>(getActivity(),
-                                                    R.layout.list_item_forecast,
-                                                    R.id.list_item_forecast_textview,
-                                                    weekForecast);
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weekForecast);
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         //Get the ref to listview widget, and add adapter to it
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
 
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        try {
+            String baseURL = "http://api.openweathermap.org/data/2.5/forecast/daily?q=560102&mode=json&units=metric&cnt=7";
+            String apiKey = "&appid=b1b15e88fa797225412429c1c50c122a";
+            URL url = new URL(baseURL + apiKey);
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream is = urlConnection.getInputStream();
+            if (is == null) {
+                return null;
+            }
+            InputStreamReader isr = new InputStreamReader(is);
+            reader = new BufferedReader(isr);
+
+            String line;
+            StringBuffer bufferString = new StringBuffer();
+            while ((line = reader.readLine()) != null) {
+                bufferString.append(line).append("\n");
+            }
+
+            if (bufferString.length() == 0) {
+                return null;
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error");
+            return null;
+        }finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    Log.e(LOG_TAG, "Error closing BufferedReader");
+                }
+            }
+        }
         //finally return the fragment rootview
         return rootView;
     }
 }
+	
